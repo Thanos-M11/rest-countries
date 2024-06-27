e27c8fbd2a9cfe82a8c7a8b4dca118656ff54475
Thanos-M11 <kalaitzisa@gmail.com>
2024-06-27 14:57:00 +0300

    update countryItem.jsx

--- src/components/CountryItem.jsx
+++ src/components/CountryItem.jsx
@@ -3,10 +3,12 @@ import styles from "./CountryItem.module.css";
 function CountryItem({ country }) {
   const { name, population, region, capital, flags } = country;
 
+  const flagAlt = country.flags.alt || `flag of ${name.common}`;
+
   return (
     <li className={styles.cartItem}>
       <div className={styles.imgContainer}>
-        <img className={styles.cartItemImg} src={flags.svg} alt={flags.alt} />
+        <img className={styles.cartItemImg} src={flags.svg} alt={flagAlt} />
       </div>
       <ul className={styles.cartItemSummary}>
         <li className={styles.cartItemHeading}>{name.common}</li>
